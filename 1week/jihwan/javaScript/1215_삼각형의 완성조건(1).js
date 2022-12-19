1ea98efa0c1140ba69949ca897bde5781abddab5
Anjgmn <jermee315@gmail.com>
2022-12-19 12:12:38 +0900

    Revert "Merge branch 'master' into Jungmin"
    This reverts commit 9e66e0549027b8b932d3cb72e04aafe290450c63, reversing
    changes made to 62a5df375cfad4d8b95b4ecf2bcbaa048876e005.

--- 1week/jihwan/javaScript/1215_삼각형의 완성조건(1).js
+++
@@ -1,9 +0,0 @@
-// #  프로그래머스 Level 0 1215_삼각형의 완성조건(1)
-// https://school.programmers.co.kr/learn/courses/30/lessons/120889?language=javascript
-
-function solution(sides) {
-  let sidesSort = sides.sort(function (a, b) {
-    return a - b;
-  });
-  return sidesSort[0] + sidesSort[1] > sidesSort[2] ? 1 : 2;
-}
