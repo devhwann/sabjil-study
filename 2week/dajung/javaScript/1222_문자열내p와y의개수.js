35e4b2da820e0be6dd954051ac770983f1ef540d
Vicpillon <dew0489@naver.com>
2022-12-22 21:51:26 +0900

    1222 commit(미완)

---
+++ 2week/dajung/javaScript/1222_문자열내p와y의개수.js
@@ -0,0 +1,14 @@
+//프로그래머스 level 1
+//https://school.programmers.co.kr/learn/courses/30/lessons/12916
+
+//문자열 s 입력
+//대소문자 구별 x
+function solution(s){
+    //resuce함수로 객체 반환
+    let object = s.toLowerCase().split("").reduce((obj, char)=>{
+        if(char === 'p') obj['p']++;
+        else if(char === 'y') obj['y']++; 
+        return obj;
+    }, {p:0, y:0})
+    return object.p === object.y ? true : false;
+}
