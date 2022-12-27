14f7088f8bfe7b2a879d6ccd91eb70c06f5e9ddb
Dajung <70261150+Vicpillon@users.noreply.github.com>
2022-12-28 00:43:29 +0900

    Delete tempCodeRunnerFile.js
    실수로 추가한 파일 삭제

--- 3week/dajung/javaScript/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-
-function solution(s) {
-    return s.toLowerCase().split("").reduce((sum, c, idx)=>{
-        if ((s[idx-1] === " " && c !== " ") || s.indexOf(c) === 0)//단어의 첫 문자일 조건
-            return sum += c.toUpperCase()
-        return sum += c
-    }, "")
-}
