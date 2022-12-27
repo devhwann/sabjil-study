ed6a7a59595c545c6f1166a5000544402e962676
Hwan97 <hg_07@naver.com>
2022-12-27 23:58:21 +0900

    1227_가장 큰 수 찾기

---
+++ 3week/jihwan/python/1227_가장 큰 수 찾기.py
@@ -0,0 +1,6 @@
+# 프로그래머스 Level 0 1227_가장 큰 수 찾기
+# https://school.programmers.co.kr/learn/courses/30/lessons/12932
+
+def solution(array):    
+    
+    return [max(array), array.index(max(array))]
